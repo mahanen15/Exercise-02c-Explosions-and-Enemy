2a82d43bd68570848ea5a88e76472bed98a40dac
mahanen15 <mahanen@iu.edu>
2022-09-21 13:17:40 -0400

    Exercise Complete

--- Player/Bullet.gd
+++ Player/Bullet.gd
@@ -4,6 +4,8 @@ var velocity = Vector2.ZERO
 var speed = 500.0
 var damage = 1
 
+var Explosion = load ("res://Effects/Explosion.tscn")
+var Effects = null
 
 func _ready():
 	velocity = Vector2(0,-speed).rotated(rotation)
@@ -14,8 +16,14 @@ func _physics_process(_delta):
 	position.y = wrapf(position.y, 0, 600)
 	
 
-
 func _on_Area2D_body_entered(body):
+	if body.has_method("damage"):
+		 body.damage(damage)
+	Effects = get_node_or_null("/root/Game/Effects")
+	if Effects != null:
+		var explosion = Explosion.instance()
+		Effects.add_child(explosion)
+	Explosion.global_position = global_position
 	queue_free()
 
 
--- Player/Player.gd
+++ Player/Player.gd
@@ -5,6 +5,10 @@ var velocity = Vector2.ZERO
 var rotation_speed = 5.0
 var speed = 5.0
 var max_speed = 400.0
+var health = 10
+
+var Effects = null
+onready var Explosion = load("res://Effects/Explosion.tscn")
 
 
 onready var Bullet = load("res://Player/Bullet.tscn")
@@ -28,8 +32,6 @@ func _physics_process(_delta):
 			bullet.rotation = rotation
 			Effects.add_child(bullet)
 
-
-
 func get_input():
 	var to_return = Vector2.ZERO
 	$Exhaust.hide()
@@ -42,4 +44,20 @@ func get_input():
 		rotation_degrees = rotation_degrees + rotation_speed
 	return to_return.rotated(rotation)
 	
+func damage(d):
+	health -= d
+	if health <= 0:
+		Effects = get_node_or_null("/root/Game/Effects")
+		if Effects != null:
+			var explosion = Explosion.instance()
+			Effects.add_child(explosion)
+			explosion.global_position = global_position
+			hide()
+			yield(explosion, "animation_finished")
+			queue_free()
+
+
+func _on_Area2D_body_entered(body):
+	if body.name != "Player" :
+		damage (100)
 		
